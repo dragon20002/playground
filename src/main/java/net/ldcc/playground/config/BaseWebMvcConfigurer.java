fa9f69f0851b4f62e9d238b3f37a1681813a063a
dragon20002 <dragon20002@naver.com>
2020-11-26 22:28:56 +0900

    [ADD] Swagger 3.0

--- src/main/java/net/ldcc/playground/config/BaseWebMvcConfigurer.java
+++ src/main/java/net/ldcc/playground/config/BaseWebMvcConfigurer.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -34,4 +35,8 @@ public class BaseWebMvcConfigurer implements WebMvcConfigurer {
         		.addPathPatterns("/api/members/**", "/api/login/get-user-info");
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/swagger-ui", "/swagger-ui/index.html");
+    }
 }
